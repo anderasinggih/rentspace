a83ffe130748e24254b6aa957da1432bd82ba87c
Andera Singgih Pratama <anderasinggih1@gmail.com>
2026-04-22 10:59:10 +0700

    3

--- database/migrations/2026_04_15_131820_add_category_id_to_units_table.php
+++ database/migrations/2026_04_15_131820_add_category_id_to_units_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('units', function (Blueprint $table) {
-            //
+            $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null');
         });
     }
 
@@ -22,7 +22,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('units', function (Blueprint $table) {
-            //
+            $table->dropForeign(['category_id']);
+            $table->dropColumn('category_id');
         });
     }
 };
