0b88348bc33d33a9d51e4170ad90439e69623d6b
Andera Singgih Pratama <anderasinggih1@gmail.com>
2026-04-26 07:36:15 +0700

    fix: correctly link radar device arrow to search by booking code on transactions page

--- resources/views/livewire/admin/radar-devices.blade.php
+++ resources/views/livewire/admin/radar-devices.blade.php
@@ -68,7 +68,7 @@
                                     </p>
                                     <p class="text-[9px] font-medium text-white/20 mt-1 leading-none">{{ str_replace('ago', '', $device['last_seen']) }}</p>
                                 </div>
-                                <a href="{{ route('admin.transactions', ['search' => $device['rental_id'] ?? $device['id']]) }}" 
+                                <a href="{{ route('admin.transactions', ['search' => $device['booking_code']]) }}" 
                                    @click.stop
                                    class="p-1 -mr-1 hover:bg-white/10 rounded-full transition-colors text-white/10 group-hover:text-white/40">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
